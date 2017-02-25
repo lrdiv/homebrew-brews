29d88908dace0e27c7f30a7f2630c92c2ca97542
Lawrence Davis <lawrence@lrdiv.co>
2017-02-25 01:10:06 -0600

    Fix SHA

--- Formula/itermocil.rb
+++ Formula/itermocil.rb
@@ -2,7 +2,7 @@ class Itermocil < Formula
   # desc "iTermocil allows you setup pre-configured layouts of panes in iTerm2."
   homepage "https://github.com/lrdiv/itermocil"
   url "https://github.com/lrdiv/itermocil/archive/0.2.2.tar.gz"
-  sha256 "c3c57306ae4adc6b688b600b88014d551f4f04ba"
+  sha256 "d61284f8de5a48063420eea4fe09f4bca9fd67539fafe91c14d17e8948845dc1"
 
   resource "PyYAML" do
     url "https://pypi.python.org/packages/source/P/PyYAML/PyYAML-3.11.tar.gz"
